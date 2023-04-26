2df881514a710b0125f24ac192b14108da46363b
Eduardo Alvarado Quirós <94879615+EADQ@users.noreply.github.com>
2023-04-26 08:16:47 -0600

    First Commit

---
+++ libreta_clientes.py
@@ -0,0 +1,126 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+import sqlite3
+
+root = Tk()
+root.title('Hola Mundo: CRM')
+
+#CREANDO CONEXION Y NOMBRE DE NUESTRA BASE DE DATOS
+conn = sqlite3.connect('crm.db')
+
+#CREANDO EL CURSOR DONDE VAMOS A REALIZAR NUESTRAS CONSULTAS A LA BASE DE DATOS
+c = conn.cursor()
+
+#CREANDO LA TABLA DONDE VAMOS A GENERAR TODOS LOS REGISTROS
+c.execute("""
+          CREATE TABLE if not exists cliente(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          nombre TEXT NOT NULL,
+          telefono TEXT NOT NULL,
+          empresa TEXT NOT NULL
+          );
+""")
+
+#CREANDO LAS FUNCIONES DE LA APLICACION
+
+def render_clientes():
+
+    rows = c.execute("SELECT * FROM cliente").fetchall()
+
+    tree.delete(*tree.get_children())
+
+    for row in rows:
+        tree.insert('', END, row[0], values=(row[1], row[2], row[3]))
+
+def insertar(cliente):
+    c.execute("""
+              INSERT INTO cliente (nombre, telefono, empresa) VALUES (?,?,?)
+              """, (cliente['nombre'], cliente['telefono'], cliente['empresa']))
+    conn.commit()
+    render_clientes()
+
+def nuevo_cliente():
+
+    def guardar():
+        if not nombre.get():
+            messagebox.showerror('Error','El nombre es obligatorio')
+            return
+
+        if not telefono.get():
+            messagebox.showerror('Error','El teléfono es obligatorio')
+            return
+
+        if not empresa.get():
+            messagebox.showerror('Error','El campo empresa es obligatorio')
+            return
+
+        cliente = {
+            'nombre': nombre.get(),
+            'telefono': telefono.get(),
+            'empresa': empresa.get()
+        }
+
+        insertar(cliente)
+        top.destroy()
+
+    top = Toplevel()
+    top.title('Nuevo cliente')
+
+    lnombre = Label(top, text='Nombre')
+    nombre = Entry(top, width=40)
+    lnombre.grid(row=0, column=0)
+    nombre.grid(row=0, column=1)
+
+    ltelefono = Label(top, text='Teléfono')
+    telefono = Entry(top, width=40)
+    ltelefono.grid(row=1, column=0)
+    telefono.grid(row=1, column=1)
+
+    lempresa = Label(top, text='Empresa')
+    empresa = Entry(top, width=40)
+    lempresa.grid(row=2, column=0)
+    empresa.grid(row=2, column=1)
+
+    guardar = Button(top, text='Guardar', command=guardar)
+    guardar.grid(row=3, column=1)
+
+    top.mainloop()
+
+def eliminar_cliente():
+
+    id = tree.selection()[0]
+    
+    cliente = c.execute("SELECT * FROM cliente WHERE id = ?", (id,)).fetchone()
+    respuesta = messagebox.askokcancel('Confirmación?' ' Estas seguro de querer eliminar el cliente?' + cliente[1] + '?')
+    if respuesta:
+        c.execute("DELETE FROM cliente WHERE id = ?", (id,))
+        conn.commit()
+        render_clientes()
+    else:
+        pass
+
+#CREANDO ELEMENTOS VISUALES
+btn = Button(root, text='Nuevo Cliente', command=nuevo_cliente)
+btn.grid(column=0, row=0)
+
+btn_eliminar = Button(root, text='Eliminar Cliente', command=eliminar_cliente)
+btn_eliminar.grid(column=1, row=0)
+
+#AGREGANDO LA TABLA DONDE VAMOS A IR AGREGANDO A NUESTROS CLIENTES
+tree = ttk.Treeview(root)
+tree['columns'] = ('Nombre', 'Teléfono', 'Empresa')
+tree.column('#0', width=0, stretch=NO)
+tree.column('Nombre')
+tree.column('Teléfono')
+tree.column('Empresa')
+
+#AGREGANDO LOS HEADINGS
+tree.heading('Nombre', text='Nombre')
+tree.heading('Teléfono', text='Teléfono')
+tree.heading('Empresa', text='Empresa')
+tree.grid(column=0, row=1, columnspan=2)
+
+render_clientes()
+root.mainloop()
+
